9b670c890a97c65c79dabddbbe9cea1dfdf61cf6
Rajani Kokkula <rkokkula@paypal.com>
2014-08-25 13:04:01 -0700

    Changes to support more Selendriod intergation needs
    * Add support for more -aut and apk in LocalSelendroidNode
    * Code that handles running of the Mobile Web Test and no apk files
    * Selecting the device using the serial number for execution of tests on a particular device
    * Resolve merge problems after rebase from develop
    * Add license header to LocalSendroidNode

--- client/src/main/java/com/paypal/selion/platform/grid/LocalSelendroidNode.java
+++ client/src/main/java/com/paypal/selion/platform/grid/LocalSelendroidNode.java
@@ -1,17 +1,29 @@
+/*-------------------------------------------------------------------------------------------------------------------*\
+|  Copyright (C) 2014 eBay Software Foundation                                                                        |
+|                                                                                                                     |
+|  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
+|  with the License.                                                                                                  |
+|                                                                                                                     |
+|  You may obtain a copy of the License at                                                                            |
+|                                                                                                                     |
+|       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
+|                                                                                                                     |
+|  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
+|  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
+|  the specific language governing permissions and limitations under the License.                                     |
+\*-------------------------------------------------------------------------------------------------------------------*/
+
 package com.paypal.selion.platform.grid;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Handler;
-//import java.util.logging.Handler;
 import java.util.logging.Level;
-//import java.util.logging.Logger;
-
-
 import java.util.logging.Logger;
 
 import org.apache.commons.io.IOUtils;
@@ -155,11 +167,22 @@ public class LocalSelendroidNode implements LocalServerComponent {
         args.add("http://127.0.0.1:" + Config.getIntConfigProperty(ConfigProperty.SELENIUM_PORT) + "/grid/register");
         args.add("-port");
         args.add(Integer.toString(port));
+        args.add("-selendroidServerPort");
+        args.add(Integer.toString(Config.getIntConfigProperty(ConfigProperty.SELENDROID_SERVER_PORT)));
 
         String autFolder = Config.getConfigProperty(ConfigProperty.SELENIUM_NATIVE_APP_FOLDER);
-        if (autFolder != null && !autFolder.trim().isEmpty()) {
-            args.add("-aut");
-            args.add(autFolder + "/paypal-here-2-0-0-2037-qa-qa.apk");
+        if ((autFolder != null) && (!autFolder.trim().isEmpty())) {
+            File folder = new File(autFolder);
+            File[] fList = folder.listFiles();
+            if (fList != null) {
+                for (File file : fList) {
+                    if ((file.isFile()) && (file.getName().endsWith(".apk"))
+                            && (!file.getName().startsWith("resigned")) && (!file.isDirectory())) {
+                        args.add("-aut");
+                        args.add(file.getAbsolutePath());
+                    }
+                }
+            }
         }
 
         args.add("-proxy");
