02598f7271095ca7ff696c902592a12a8f9b7a49
Doug Simmons <dsimmons@paypal.com>
2014-09-19 12:57:45 -0700

    Change Travis CI config for PR's originating from forked repos

---
+++ run-travis-ci-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ev
+if [ "${TRAVIS_REPO_SLUG}" = "paypal/SeLion" ]; then
+  echo { \"sauceUserName\": \"${SAUCE_USERNAME}\", \"sauceApiKey\": \"${SAUCE_ACCESS_KEY}\", \"tunnel-identifier\": \"__string__${TRAVIS_JOB_NUMBER}\", \"build\": \"${TRAVIS_BUILD_NUMBER}\", \"idle-timeout\": 120, \"tags\": [\"commit ${TRAVIS_COMMIT}\", \"branch ${TRAVIS_BRANCH}\", \"pull request ${TRAVIS_PULL_REQUEST}\"] } > client/src/test/resources/sauceConfig.json
+  mvn test -Djava.net.preferIPv4Stack=true
+else
+  mvn test -Djava.net.preferIPv4Stack=true -DsuiteXmlFile=PhantomJS-Suite.xml -DSELION_SELENIUM_RUN_LOCALLY=true
+fi
