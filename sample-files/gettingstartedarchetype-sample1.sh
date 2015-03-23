6734e867989582f16a11b8c5fbd5b7d5c60e68b9
Doug Simmons <dsimmons@paypal.com>
2015-03-22 22:50:34 -0700

    Tweaks to the SeLion archetype docs

--- sample-files/gettingstartedarchetype-sample1.sh
+++ sample-files/gettingstartedarchetype-sample1.sh
@@ -1,3 +1,8 @@
-mvn archetype:generate -B -DartifactId=Sample -Dversion=1.0.0-SNAPSHOT \
- -DgroupId=com.mycompany.myproject -DarchetypeGroupId=com.paypal.selion -DarchetypeArtifactId=SeLion-Archetype \
- -DarchetypeVersion=1.0.0-SNAPSHOT -DarchetypeRepository=https://oss.sonatype.org/content/repositories/snapshots
+mvn archetype:generate -B \
+ -DartifactId=Sample \
+ -Dversion=1.0.0-SNAPSHOT \
+ -DgroupId=com.mycompany.myproject \
+ -DarchetypeGroupId=com.paypal.selion \
+ -DarchetypeArtifactId=SeLion-Archetype \
+ -DarchetypeVersion=1.0.0-SNAPSHOT \
+ -DarchetypeRepository=https://oss.sonatype.org/content/repositories/snapshots
