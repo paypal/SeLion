8576c9c3edce0383554d3a0bc0ee52f62bbd636c
Doug Simmons <dsimmons@paypal.com>
2015-04-06 13:50:17 -0700

    Minor changes to EventListener docs

--- sample-files/MyEventListener.java
+++ sample-files/MyEventListener.java
@@ -6,17 +6,19 @@ import org.openqa.selenium.remote.DriverCommand;
 import org.testng.Reporter;
 
 /**
- *
+ * An Example EventListner which simply logs to TestNG's reporter
  */
 public class MyEventListener implements EventListener {
   /**
-   * This method will be called by {@link EventFiringCommandExecutor} BEFORE executing each selenium command.
+   * This method will be called by {@link EventFiringCommandExecutor} BEFORE
+   * executing each selenium command.
    *
-   * @param command - A {@link Command} that represents the command being executed by
-   *                Selenium.
-   *                In order to filter out specific commands you may extract the actual command via
-   *                {@link Command#getName()} and then compare it with the predefined set of
-   *                commands available as strings in {@link DriverCommand}
+   * @param command - A {@link Command} that represents the command being
+   *                executed by Selenium. In order to filter out specific
+   *                commands you may extract the actual command via {@link
+   *                Command#getName()} and then compare it with the predefined
+   *                set of commands available as strings in {@link
+   *                DriverCommand}
    */
   @Override
   public void beforeEvent(Command command) {
@@ -27,13 +29,15 @@ public class MyEventListener implements EventListener {
   }
 
   /**
-   * This method will be called by {@link EventFiringCommandExecutor} AFTER executing each selenium command.
+   * This method will be called by {@link EventFiringCommandExecutor} AFTER
+   * executing each selenium command.
    *
-   * @param command - A {@link Command} that represents the command being executed by
-   *                Selenium.
-   *                In order to filter out specific commands you may extract the actual command via
-   *                {@link Command#getName()} and then compare it with the predefined set of
-   *                commands available as strings in {@link DriverCommand}
+   * @param command - A {@link Command} that represents the command being
+   *                executed by Selenium. In order to filter out specific
+   *                commands you may extract the actual command via {@link
+   *                Command#getName()} and then compare it with the predefined
+   *                set of commands available as strings in {@link
+   *                DriverCommand}
    */
   @Override
   public void afterEvent(Command command) {
