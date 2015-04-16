b9a02ae99a722aca54168f55eea50ef44408ed1c
Sravanthi Konduru <srkonduru@paypal.com>
2015-04-16 13:12:27 -0500

    Addressed #49. Update docs for selendroid based mobile tests

--- sample-files/gettingstartedmobile-sample2.java
+++ sample-files/gettingstartedmobile-sample2.java
@@ -11,26 +11,28 @@ import com.paypal.selion.annotations.MobileTest;
 import com.paypal.selion.platform.grid.Grid;
 import com.paypal.selion.reports.runtime.MobileReporter;
 
-public class MobileDemo {
+public class IOSMobileDemo {
 
-    @MobileTest(appName = "InternationalMountains")
-    @Test
-    public void testMethod() throws InterruptedException {
-        MobileReporter.log("My Screenshot 1", true);
-        List cells = Grid.iOSDriver().findElements(By.className("UIATableCell"));
-        Assert.assertEquals(9, cells.size());
+  @MobileTest(appName = "InternationalMountains", mobileNodeType="ios-driver", 
+    device = "iphone")
+  @Test
+  public void testMethod() throws InterruptedException {
+    MobileReporter.log("My Screenshot 1", true);
+    List<WebElement> cells = Grid.driver()
+      .findElements(By.className("UIATableCell"));
+    Assert.assertEquals(9, cells.size());
 
-        // get the 1st mountain
-        WebElement first = (WebElement) cells.get(0);
-        first.click();
-        Thread.sleep(10 * 1000);
+    // get the 1st mountain
+    WebElement first = (WebElement) cells.get(0);
+    first.click();
+    Thread.sleep(10 * 1000);
 
-        // take a screenshot using the normal selenium api.
-        MobileReporter.log("My Screenshot 2", true);
+    // take a screenshot using the normal selenium api.
+    MobileReporter.log("My Screenshot 2", true);
 
-        // access the content
-        By selector = By.xpath("//UIAStaticText[contains(@name,'climbed')]");
-        WebElement text = Grid.iOSDriver().findElement(selector);
-        System.out.println(text.getAttribute("name"));
-    }
+    // access the content
+    By selector = By.xpath("//UIAStaticText[contains(@name,'climbed')]");
+    WebElement text = Grid.driver().findElement(selector);
+    System.out.println(text.getAttribute("name"));
+  }
 }
---
+++ sample-files/gettingstartedmobile-sample3.java
@@ -0,0 +1,30 @@
+package com.mycompany.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.paypal.selion.annotations.MobileTest;
+import com.paypal.selion.platform.grid.Grid;
+import com.paypal.selion.reports.runtime.MobileReporter;
+
+public class AndroidMobileDemo {
+
+  @Test
+  @MobileTest(appName = "io.selendroid.testapp", device = "android:19", 
+    mobileNodeType = "selendroid")
+  public void testMethod() throws Exception {
+    MobileReporter.log("My Screenshot 1", true);
+    WebElement inputField = Grid.driver().findElement(
+        By.id("my_text_field"));
+
+    // verify TextField is enabled
+    Assert.assertEquals("true", inputField.getAttribute("enabled"));
+    MobileReporter.log("My Screenshot 2", true);
+    inputField.sendKeys("Selendroid");
+
+    // verify content in the TextField
+    Assert.assertEquals("Selendroid", inputField.getText());
+  }
+}
