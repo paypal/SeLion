b8dc9279183955828efaa1d2f0fadbfbfba0f9e2
Rene Lux <rene.lux@symbio.com>
2015-04-16 10:13:41 +0800

    Add documentation for the customElements option in the SeLion-Code-Generator

---
+++ sample-files/whatIsCodeGenerator-sample7.java
@@ -0,0 +1,24 @@
+package com.mycompany;
+
+import com.paypal.selion.platform.html.AbstractElement;
+import com.paypal.selion.platform.html.ParentTraits;
+
+public class CustomElementOne extends AbstractElement {
+
+  public CustomElementOne(ParentTraits parent, String locator) {
+    super(parent, locator);
+  }
+
+  public CustomElementOne(String locator, String controlName, 
+    ParentTraits parent) {
+    super(locator, controlName, parent);
+  }
+
+  public CustomElementOne(String locator, String controlName) {
+    super(locator, controlName);
+  }
+
+  public CustomElementOne(String locator) {
+    super(locator);
+  }
+}
