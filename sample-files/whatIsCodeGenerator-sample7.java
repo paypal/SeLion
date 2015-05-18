14de75edcd366e2562577074bd530d19db5203d3
Yuvha Secaran <ysecaran@paypal.com>
2015-05-18 13:59:59 +0530

    Updated Code generator documentation
    Add more content related to our support of custom elements for mobile

--- sample-files/whatIsCodeGenerator-sample7.java
+++ sample-files/whatIsCodeGenerator-sample7.java
@@ -10,7 +10,7 @@ public class CustomElementOne extends AbstractElement {
   }
 
   public CustomElementOne(String locator, String controlName, 
-    ParentTraits parent) {
+                          ParentTraits parent) {
     super(locator, controlName, parent);
   }
 
