ef0b307d4227a3bb04a012d6a9e43ce63bffbdaa
Yuvha Secaran <ysecaran@paypal.com>
2015-05-28 12:08:33 +0530

    Adding unit tests for SeLion-Grid
    - Added tests for FileExtractor , ConfigParser , AuthenticationHelper,
      SauceConfigReader, FileDownloader and ArtifactDetails.
    - Updated .gitattributes file

--- server/src/test/java/com/paypal/selion/grid/FileDownloaderTest.java
+++ server/src/test/java/com/paypal/selion/grid/FileDownloaderTest.java
@@ -15,16 +15,60 @@
 
 package com.paypal.selion.grid;
 
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.SystemUtils;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.paypal.selion.grid.FileDownloader;
+import com.paypal.selion.pojos.SeLionGridConstants;
 
 public class FileDownloaderTest {
 
+    File downloadedFile = null;
+    File homeDir = null;
+    
+    @BeforeClass
+    public void createHomeDir() throws IOException{
+        //Re-setting the path -just in case if any other test changes the SeLion_home_dir constant
+        SeLionGridConstants.SELION_HOME_DIR= SystemUtils.USER_HOME + "/.selion/";
+        homeDir = new File(SeLionGridConstants.SELION_HOME_DIR+"/downloads/");
+        homeDir.mkdirs();
+    }
+    
     @Test(expectedExceptions = { UnsupportedOperationException.class })
     public void testUnsupportedFileType() {
         // gz compression type is not supported.
         String unsupportedFileURL = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-i686.tar.gz";
         FileDownloader.downloadFile(unsupportedFileURL, "123CEFRGxSdfnsfwefla");
+    }
+
+    @Test
+    public void testFileDownload() {
+        String testUrl = "https://selenium-release.storage.googleapis.com/2.45/IEDriverServer_Win32_2.45.0.zip";
+        String tempChecksum = "dde210e04e5c1b0d6019fd8a1199df18";
+        String result = FileDownloader.downloadFile(testUrl, tempChecksum);
+        downloadedFile = new File(result);
+        assertTrue(downloadedFile.exists());
+    }
+    
+    @Test
+    public void testInvalidChecksum() {
+        String testUrl = "https://chromedriver.storage.googleapis.com/2.14/chromedriver_win32.zip";
+        String tempChecksum = "dde210e04e5c1b0d6019fd8a1199df18";
+        String result = FileDownloader.downloadFile(testUrl, tempChecksum);
+        assertNull(result);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanUpFile() {
+      FileUtils.deleteQuietly(downloadedFile);
+      FileUtils.deleteQuietly(homeDir);
     }
 }
