d603cf803c478ea55e9b1f9227a537c416506476
Rajendran Sundaram <rajsundaram@paypal.com>
2016-01-21 17:33:07 +0530

    Added documentation about the multiple ways to define additonal selenium capabilties

---
+++ sample-files/additionalseleniumcapabilitiesviawebtest-sample.java
@@ -0,0 +1,21 @@
+package com.mycompany.test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.paypal.selion.annotations.WebTest;
+import com.paypal.selion.platform.grid.Grid;
+
+/**
+ * This test demonstrates how to add additional Selenium capabilities via WebTest annotation
+ */
+public class AdditionalSeleniumCapabilitiesViaWebTest {
+
+  @Test
+  @WebTest(additionalCapabilities = { "useBooleanCaps:true","key:value" })
+  public void testCapabilityViaAnnotation() {
+    assertEquals(Grid.getWebTestSession().getAdditionalCapabilities().getCapability("useBooleanCaps"), Boolean.TRUE);
+    assertEquals(Grid.getWebTestSession().getAdditionalCapabilities().getCapability("key"), "value");
+  }
+}
