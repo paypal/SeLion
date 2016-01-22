7ff68a82010b8183aa663c9b972109ba6287fe93
Doug Simmons <dsimmons@paypal.com>
2016-01-22 15:07:38 -0800

    [SQUASHME] Updates / Edits

--- sample-files/additionalseleniumcapabilitiesviawebtest-sample.java
+++ sample-files/additionalseleniumcapabilitiesviawebtest-sample.java
@@ -8,14 +8,18 @@ import com.paypal.selion.annotations.WebTest;
 import com.paypal.selion.platform.grid.Grid;
 
 /**
- * This test demonstrates how to add additional Selenium capabilities via WebTest annotation
+ * This test demonstrates how to add additional Selenium capabilities via
+ * the WebTest annotation. MobileTest has the exact same property
+ * "additionalCapabilities"
  */
 public class AdditionalSeleniumCapabilitiesViaWebTest {
 
   @Test
   @WebTest(additionalCapabilities = { "useBooleanCaps:true","key:value" })
   public void testCapabilityViaAnnotation() {
-    assertEquals(Grid.getWebTestSession().getAdditionalCapabilities().getCapability("useBooleanCaps"), Boolean.TRUE);
-    assertEquals(Grid.getWebTestSession().getAdditionalCapabilities().getCapability("key"), "value");
+    assertEquals(Grid.getWebTestSession().getAdditionalCapabilities()
+      .getCapability("useBooleanCaps"), Boolean.TRUE);
+    assertEquals(Grid.getWebTestSession().getAdditionalCapabilities()
+      .getCapability("key"), "value");
   }
 }
