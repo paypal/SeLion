69f5a6c9b5995b96ecd764983f959c085d9ceeb1
Doug Simmons <dsimmons@paypal.com>
2016-02-29 16:08:48 -0800

    Update travis ci build process/scripts
    - Renamed run-travis-ci-tests.sh
    - merged travis-config-for-javadoc-push.sh and
      push-javadoc-to-gh-pages.sh into
      travis-push-javadoc-to-gh-pages.sh
    - updated logic for pushing javadocs to gh-pages
      such that it is not dependent on a separate
      git clone -- e.g. $HOME/SeLion. Now is uses
      the same git clone ($HOME/$repo/SeLion) that
      the build is done on and instead sets up a
      remote tracking branch against a
      'writable' remote
    - bump maven javadoc plugin version
    - Add logic in sonatype deploy script to check
      current active git branch is 'develop'

--- utilities/travis-deploy-to-sonatype.sh
+++ utilities/travis-deploy-to-sonatype.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-#Deploys resulting artifacts to sonatype nexus
+################################################
+# Deploys resulting artifacts to sonatype nexus
+################################################
+
+# Deploy only if the following conditions are satisfied
+# 1. The build is for the project paypal/SeLion, not on the fork
+# 2. The project is built on oraclejdk7
+# 3. The build is not on a pull request
+# 4. The build is on the develop branch
 if [ "$TRAVIS_REPO_SLUG" = "paypal/SeLion" ] && [ "$TRAVIS_JDK_VERSION" = "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "develop" ]; then
     echo "Deploying to Sonatype...\n"
-    mvn -s utilities/settings.xml deploy -B -V
+    # checkout the develop branch
+    git checkout develop
+    # verify that we are on develop branch, otherwise exit with error code
+    output=$(git rev-parse --abbrev-ref HEAD)
+    if [ "$output" = "develop" ]; then
+        # 'clean' must also be invoked here or the SeLion-Archetype build will fail
+        # due to the previous output from the maven-invoker-plugin 
+        mvn clean deploy -DskipTests=true --settings utilities/settings.xml -B -V
+    else
+        echo "Failed to switch into develop branch."
+        exit 1
+    fi
+else
+    echo "Deployment selection criteria not met."
 fi
--- utilities/travis-run-ci-tests.sh
+++ utilities/travis-run-ci-tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -ev
+#########################################################
+# This script runs the tests either locally with 
+# phantomjs or remotely against sauce labs depending on
+# CI configuration / presence of environment variables
+#########################################################
+
 if [ -n "${SAUCE_USERNAME}" ]; then
   echo { \"sauceUserName\": \"${SAUCE_USERNAME}\", \"sauceApiKey\": \"${SAUCE_ACCESS_KEY}\", \"tunnel-identifier\": \"__string__${TRAVIS_JOB_NUMBER}\", \"build\": \"${TRAVIS_BUILD_NUMBER}\", \"idle-timeout\": 120, \"tags\": [\"commit ${TRAVIS_COMMIT}\", \"branch ${TRAVIS_BRANCH}\", \"pull request ${TRAVIS_PULL_REQUEST}\"] } > client/src/test/resources/sauceConfig.json
   mvn test -DSELION_BROWSER_CAPABILITY_PLATFORM=WINDOWS -B -V
