3145bf6bb2b4e7af87baf5254dbac3658620f15e
Ali Kia <akia@ebay.com>
2016-09-06 16:58:37 +0800

    decrease test suite parameters
    1. removing mobilePlatform parameter and getting it from mobileTestSession
    2. updating mobileDeviceType to be optional and can be resolved from mobileDevice

--- client/src/main/java/com/paypal/selion/testcomponents/AbstractPage.java
+++ client/src/main/java/com/paypal/selion/testcomponents/AbstractPage.java
@@ -102,7 +102,9 @@ public abstract class AbstractPage implements WebPage {
         if (session != null && StringUtils.isNotBlank(session.getXmlTestName())) {
             LocalConfig lc = ConfigManager.getConfig(session.getXmlTestName());
             site = lc.getConfigProperty(ConfigProperty.SITE_LOCALE);
-            platform = WebDriverPlatform.valueOf(lc.getConfigProperty(ConfigProperty.MOBILE_PLATFORM).toUpperCase());
+            if (Grid.getMobileTestSession() != null) {
+                platform = Grid.getMobileTestSession().getPlatform();
+            }
         }
     }
 
