4967c150bfc685af13cfc4cb402eb216656d67de
Doug Simmons <dsimmons@paypal.com>
2017-11-17 18:26:12 -0800

    travisci changes - test firefox with sauce on the parent repo

--- utilities/travis-run-chrome-tests.sh
+++ utilities/travis-run-chrome-tests.sh
@@ -10,8 +10,11 @@ if [ -n "${SAUCE_USERNAME}" ]; then
   echo { \"sauceUserName\": \"${SAUCE_USERNAME}\", \"sauceApiKey\": \"${SAUCE_ACCESS_KEY}\", \"tunnel-identifier\": \"__string__${TRAVIS_JOB_NUMBER}\", \"build\": \"${TRAVIS_BUILD_NUMBER}\", \"idle-timeout\": 120, \"tags\": [\"commit ${TRAVIS_COMMIT}\", \"branch ${TRAVIS_BRANCH}\", \"pull request ${TRAVIS_PULL_REQUEST}\"] } > client/src/test/resources/sauceConfig.json
 fi
 
-mvn test -pl client -DsuiteXmlFile=Chrome-Suite.xml -DSELION_BROWSER=chrome-headless \
-  -DSELION_SELENIUM_RUN_LOCALLY=true -DSELION_DOWNLOAD_DEPENDENCIES=true \
-  -DSELION_SELENIUM_CUSTOM_CAPABILITIES_PROVIDER=com.paypal.selion.TravisCICapabilityBuilder \
-  -DBROWSER_PATH=`which google-chrome` -B -V
-exit $?
+if [ "$USE_SAUCELABS" = true ]; then
+  mvn test -pl client -B -V -DsuiteXmlFile=Chrome-Suite.xml -DSELION_BROWSER=chrome-headless && exit $?
+else
+  mvn test -pl client -B -V -DsuiteXmlFile=Chrome-Suite.xml -DSELION_BROWSER=chrome-headless \
+    -DSELION_SELENIUM_RUN_LOCALLY=true -DSELION_DOWNLOAD_DEPENDENCIES=true \
+    -DSELION_SELENIUM_CUSTOM_CAPABILITIES_PROVIDER=com.paypal.selion.TravisCICapabilityBuilder \
+    -DBROWSER_PATH=`which google-chrome` && exit $?
+fi
